196f563b0bf0802810df82e89a99619c3d33c1c2
arioshi <arioshi.game@gmail.com>
2012-02-22 09:36:55 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebRepublic::Application.config.secret_token = '28afcbf27a1f4e9e8552c444acead0fd52fe2c6c685250fb475ba524d60bec0fb2da0ece63f7d26dcf3c78b2d6ec328a2bfc1ade48dad4cdc683f9e34504ba6d'
